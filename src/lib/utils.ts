165b922a9025e87f5b12f284203cb463aa6e2316
GuimaraesSilva <fabio8silva@hotmail.com>
2025-02-26 16:56:52 +0000

    Melhorias no manuseio de módulos e relatórios de erro: - Aprimorado o manuseio de módulos na configuração do webpack - Melhoramento do relatório de erros para módulos recusados e não aceites - Atualizada a integração do Clerk para melhor gestão de sessões - Otimizado o processo de leitura de recursos e construção de módulos - Refinada a lógica de substituição de módulos a quente (HMR)

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,45 @@
 import { auth } from "@clerk/nextjs/server";
 
+// Session Claims
 const { userId, sessionClaims } = await auth();
 export const role = (sessionClaims?.metadata as { role?: string })?.role;
 export const currentUserId = userId;
+
+// Function to get the latest Monday
+const getLatestMonday = (): Date => {
+  const date = new Date();
+  const day = date.getDay();
+  const diff = date.getDate() - day + (day === 0 ? -6 : 1);
+  return new Date(date.setDate(diff));
+};
+
+// Current Work Week
+export const adjustScheduleToCurrentWeek = (
+    lessons: { title: string; start: Date; end: Date }[]
+  ): { title: string; start: Date; end: Date }[] => {
+    const latestMonday = getLatestMonday();
+  
+    return lessons.map((lesson) => {
+      const lessonDayOfWeek = lesson.start.getDay();
+      const daysFromMonday = lessonDayOfWeek === 0 ? 6 : lessonDayOfWeek - 1;
+      const adjustedStartDate = new Date(latestMonday);
+      adjustedStartDate.setDate(latestMonday.getDate() + daysFromMonday);
+      adjustedStartDate.setHours(
+        lesson.start.getHours(),
+        lesson.start.getMinutes(),
+        lesson.start.getSeconds()
+      );
+      const adjustedEndDate = new Date(adjustedStartDate);
+      adjustedEndDate.setHours(
+        lesson.end.getHours(),
+        lesson.end.getMinutes(),
+        lesson.end.getSeconds()
+      );
+  
+      return {
+        title: lesson.title,
+        start: adjustedStartDate,
+        end: adjustedEndDate,
+      };
+    });
+};
